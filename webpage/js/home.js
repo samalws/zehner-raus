d887ddb2200ee072687cfb16fbd25e113f4621a7
Jerry Huang <jerryhuang485@gmail.com>
2021-01-31 06:52:57 -0500

    literaly cancer

--- webpage/js/home.js
+++ webpage/js/home.js
@@ -1,25 +1,3 @@
-/*
-var input = document.getElementById("gameID");
-console.log(input);
-input.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-   event.preventDefault();
-   alert(document.getElementById("gameID").value);
-   gameID = document.getElementById("gameID").value
-  }
-});
-
-var input2 = document.getElementById("name");
-console.log(input2);
-input2.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-   event.preventDefault();
-   alert(document.getElementById("name").value);
-   name = document.getElementById("gameID").value
-  }
-});
-*/
-
 function getVals(){
   var gameID = document.getElementById("gameID").value
   var name = document.getElementById("name").value
@@ -27,9 +5,63 @@ function getVals(){
 }
 
 function sendVals(){
-  //sendvals to server and redirect???
   var vals = getVals();
   console.log(vals);
+  newHTML = lobbybody
+  document.querySelector('body').innerHTML = newHTML;
+}
+
+function tomain(){
+  newHTML = mainbody
+  document.querySelector('body').innerHTML = newHTML;
+
+  var nthchildindex = 6
+  var arrayOfLatestCards;
+  var pile = document.getElementsByClassName("cardpile");
+  for(var j = 0; j < 8; j++){
+    const i = j
+    //get nth childindex from arrayofLatestCards
+    leftRightOG = i % 2;
+    console.log(leftRightOG);
+    const leftRight = leftRightOG;
+    const allcards2 = pile[i].querySelectorAll('.card')
+    pile[i].onmouseover = function(){
+      for(var k = 0; k < nthchildindex; k++){
+        allcards2[k].style.opacity = 1;
+      }
+      allcards2[nthchildindex].style.position = "relative";
+      if(leftRight != 0)
+        allcards2[nthchildindex].style.marginLeft = "-10vh"; //test
+      else
+        allcards2[nthchildindex].style.marginRight = "-10vh";
+      allcards2[nthchildindex].style.opacity = 1;
+    }
+
+    pile[i].onmouseout = function() {
+      //console.log(leftRight);
+      for(var k  = 0; k < nthchildindex; k++){
+        allcards2[k].style.opacity = 0;
+      }
+      allcards2[nthchildindex].style.position = "absolute";
+      if(leftRight != 0){
+        allcards2[nthchildindex].style.marginLeft = "0"; //test
+        //allcards2[nthchildindex].style.marginLeft = "0"; //test
+        //console.log("bleh")
+      } else{
+        allcards2[nthchildindex].style.marginRight = "0";
+        allcards2[nthchildindex].style.right = "0";
+      }
+      allcards2[nthchildindex].style.opacity = 1;
+    }
+  }
 }
 
 console.log("working");
+
+var lobbybody = document.getElementById("lobby").innerHTML;
+console.log(lobbybody)
+
+document.getElementById("lobby").remove()
+
+var mainbody = document.getElementById("main").innerHTML;
+document.getElementById("main").remove()
--- webpage/js/lobby.js
+++ webpage/js/lobby.js
@@ -62,3 +62,48 @@ function ding(){
   document.getElementById('beep-sound').play()
   //TODO send ding
 }
+
+function tomain(){
+  newHTML = mainbody
+  document.querySelector('body').innerHTML = newHTML;
+
+  var nthchildindex = 6
+  var arrayOfLatestCards;
+  var pile = document.getElementsByClassName("cardpile");
+  for(var j = 0; j < 8; j++){
+    const i = j
+    //get nth childindex from arrayofLatestCards
+    leftRightOG = i % 2;
+    console.log(leftRightOG);
+    const leftRight = leftRightOG;
+    const allcards2 = pile[i].querySelectorAll('.card')
+    pile[i].onmouseover = function(){
+      for(var k = 0; k < nthchildindex; k++){
+        allcards2[k].style.opacity = 1;
+      }
+      allcards2[nthchildindex].style.position = "relative";
+      if(leftRight != 0)
+        allcards2[nthchildindex].style.marginLeft = "-10vh"; //test
+      else
+        allcards2[nthchildindex].style.marginRight = "-10vh";
+      allcards2[nthchildindex].style.opacity = 1;
+    }
+
+    pile[i].onmouseout = function() {
+      //console.log(leftRight);
+      for(var k  = 0; k < nthchildindex; k++){
+        allcards2[k].style.opacity = 0;
+      }
+      allcards2[nthchildindex].style.position = "absolute";
+      if(leftRight != 0){
+        allcards2[nthchildindex].style.marginLeft = "0"; //test
+        //allcards2[nthchildindex].style.marginLeft = "0"; //test
+        //console.log("bleh")
+      } else{
+        allcards2[nthchildindex].style.marginRight = "0";
+        allcards2[nthchildindex].style.right = "0";
+      }
+      allcards2[nthchildindex].style.opacity = 1;
+    }
+  }
+}
